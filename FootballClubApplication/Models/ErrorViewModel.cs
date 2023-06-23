67eb6aa7358addedb52771866a17ff0a490678ee
Konstantinos Kinnas <kinnaskonstantinos0@gmail.com>
2023-06-23 10:28:55 +0300

    Added The Project Files For MVC And .Net Library
    I added the project files for MVC, which I will use for my UI and for my .Net Library, which will be used for my data access layer.

---
+++ FootballClubApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FootballClubApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
